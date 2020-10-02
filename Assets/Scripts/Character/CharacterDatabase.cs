322099e0bf7b2142b667630e2c62cb57a966321e
Sakyawira <sakyawira@gmail.com>
2020-10-02 20:17:19 +1300

    fail attempt to create serializeabl dictionary, create a database with hardcoded sprites

---
+++ Assets/Scripts/Character/CharacterDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="Character Database", fileName = "New Character Database")]
+public class CharacterDatabase : ScriptableObject
+{
+    public Character[] Characters;
+   // public DictionaryOfStringAndInt EmotionSprites;
+}
