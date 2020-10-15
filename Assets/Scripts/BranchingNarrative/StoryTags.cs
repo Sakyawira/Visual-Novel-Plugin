07e3a90705ac13c6b059e360724a663270c14bdd
Sakyawira <sakyawira@gmail.com>
2020-10-16 03:52:54 +1300

    working in progress to create story tags system

---
+++ Assets/Scripts/BranchingNarrative/StoryTags.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Branch
+{
+    public List<string> Tags;
+    public string SceneName;
+}
+
+public class StoryTags : MonoBehaviour
+{
+    public List<Branch> Branches;
+
+    // Check whether the needed ingredients for a recipe are available
+    bool containsAll(List<string> _neededTags, List<string> availableTags)
+    {
+        int len = _neededTags.Count;
+        if (availableTags.Count != _neededTags.Count)
+        {
+            return false;
+        }
+
+        availableTags.Sort();//([](const Ingredient&LHS, const Ingredient&RHS) { return LHS > RHS; });
+        _neededTags.Sort();//([](const Ingredient&LHS, const Ingredient&RHS) { return LHS > RHS; });
+
+        for (int i = 0; i < len; i++)
+        {
+            /*if (_availableIngredients.Find(_neededIngredients[i]) == INDEX_NONE )*/
+            if (availableTags[i] != _neededTags[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Check if any of the recipe in the array of recipes can be made
+    public Branch haveBranch(List<string> _availableTags)
+    {
+        foreach (Branch branch in Branches)
+        {
+            if (containsAll(branch.Tags, _availableTags))
+            {
+                //recipe.IsDiscovered = true;
+                return branch;
+            }
+        }
+        return new Branch();
+    }
+}
