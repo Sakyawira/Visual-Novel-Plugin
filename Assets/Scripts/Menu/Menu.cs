c71c5d9d2884b284b312ac61280641cd30835aeb
Sakyawira <sakyawira@gmail.com>
2021-03-26 02:55:26 +1300

    unable to open achievement

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,23 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine.SceneManagement;
+
 
 public class Menu : MonoBehaviour
 {
-    GameObject storyTags;
-
-    public void Start()
+    bool isUserAuthenticated = false;
+    // Use this for initialization
+    void Start()
     {
-        storyTags = GameObject.Find("Branches");
+        PlayGamesPlatform.Activate(); // activate playgame platform
+        PlayGamesPlatform.DebugLogEnabled = true; //enable debug log
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isUserAuthenticated)
+        {
+            Social.localUser.Authenticate((bool success) => {
+                if (success)
+                {
+                    Debug.Log("You've successfully logged in");
+                    isUserAuthenticated = true; // set value to true
+                }
+                else
+                {
+                    Debug.Log("Login failed for some reason");
+                }
+            });
+        }
+    }
+
     public void NewGame()
     {
         SceneManager.LoadScene(1);
     }
     public void Continue()
     {
-        string NextLevel = storyTags.GetComponent<StoryTags>().GetNextLevel();
-        SceneManager.LoadScene(NextLevel);
+
+    }
+
+    public void OpenAchievement()
+    {
+        Social.localUser.Authenticate((bool success) => {
+            if (success)
+            {
+                Debug.Log("You've successfully logged in");
+                Social.ShowAchievementsUI();
+            }
+            else
+            {
+                Debug.Log("Login failed for some reason");
+            }
+        });
+    }
+
+    public void AddAchievement()
+    {
+        Social.ReportProgress(Achievement.achievement_eat_breakfast, 100, (bool sucsess) => { });
     }
 }
